c18e29c22dfb44d828a4c2a600d83bb75f0e351f
Ragapriyaait <75374788+Ragapriyaait@users.noreply.github.com>
2022-02-18 18:26:56 +0530

    Add files via upload

--- wpmktgengine-woocommerce.php
+++ wpmktgengine-woocommerce.php
@@ -5,7 +5,7 @@ Description: Genoo, LLC
 Author:  Genoo, LLC
 Author URI: http://www.genoo.com/
 Author Email: info@genoo.com
-Version: 1.7.42
+Version: 1.7.43
 License: GPLv2
 WC requires at least: 3.0.0
 WC tested up to: 5.2.3
@@ -4755,7 +4755,7 @@ add_action(
 
 add_action(
     'upgrader_process_complete',
-    function ($upgrader_object, $options) use ($api) {
+    function ($upgrader_object, $options) {
         global $WPME_API;
 
         $our_plugin = plugin_basename(__FILE__);
