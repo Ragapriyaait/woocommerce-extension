1cd2eff5eadcd232e6685805244b921cb69a84bb
Ragapriyaait <75374788+Ragapriyaait@users.noreply.github.com>
2022-02-18 18:54:06 +0530

    Add files via upload

--- wpmktgengine-woocommerce.php
+++ wpmktgengine-woocommerce.php
@@ -5,7 +5,7 @@ Description: Genoo, LLC
 Author:  Genoo, LLC
 Author URI: http://www.genoo.com/
 Author Email: info@genoo.com
-Version: 1.7.47
+Version: 1.7.48
 License: GPLv2
 WC requires at least: 3.0.0
 WC tested up to: 5.2.3
@@ -4969,6 +4969,11 @@ add_action(
 
                         [
                             'name' => 'cart emptied',
+
+                            'description' => '',
+                        ],
+                        [
+                            'name' => 'cart emptied1',
 
                             'description' => '',
                         ],
