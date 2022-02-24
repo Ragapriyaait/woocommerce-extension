31763789e6848a98815c3666604bad4e43c8cd46
Ragapriyaait <75374788+Ragapriyaait@users.noreply.github.com>
2022-02-24 12:22:25 +0530

    Add files via upload

--- wpmktgengine-woocommerce.php
+++ wpmktgengine-woocommerce.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 1.7.86
+    Version: 1.7.87
     License: GPLv2
     WC requires at least: 3.0.0
     WC tested up to: 5.2.3
@@ -309,6 +309,10 @@ register_activation_hook(__FILE__, function () {
                     ],
                     [
                         'name' => 'Subscription Pending Cancellation',
+                        'description' => '',
+                    ],
+                    [
+                        'name' => 'Subscription Pending Test',
                         'description' => '',
                     ],
                 ]);
